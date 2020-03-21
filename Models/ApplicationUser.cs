0ab82e171dd05303109d89ce4e6b2f6b7b13f697
R3ido101 <oliver_reid_@hotmail.co.uk>
2020-03-21 17:28:21 +0000

    Editied User Model

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,5 +8,9 @@ namespace Project_help.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public int AccountType { get; set; }
+        public string CountryCode { get; set; }
+        public int Age { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
