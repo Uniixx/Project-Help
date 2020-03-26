841fdee7ac53414f3b5f13c1e92bb164497110da
R3ido101 <oliver_reid_@hotmail.co.uk>
2020-03-26 00:34:07 +0000

    Added report model

---
+++ Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_help.Models
+{
+    public class Report
+    {
+        public User ReportedUser { get; set; }
+        public string Reporter { get; set; }
+        public string ReasonForReport { get; set; }
+        public DateTime TimeOfReport { get; set; }
+        public bool IsAssigned { get; set; }
+        public string Assignee { get; set; }
+        public bool IsComplete { get; set; }
+        public int Status { get; set; } // example 1 = open 2 = being reviewed -1 = closed and set the bool to true.
+        public byte Video { get; set; }
+    }
+}
